e864fc0baf06b7b682e680f5ed457c1e971fcbc0
Ryan Mason <ryanmasonmusic@gmail.com>
2022-12-06 20:57:10 -0500

    commit tree 1333a83006bbd7ff43ff9dffb88dc5793cbdbb2b

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// function getTotalAmountForBatteries(batteryBatches) {
+//     var totalBatteries = 0;
+
+//     for (const batteries of batteryBatches) {
+//         totalBatteries += batteries.batteries;
+//     };
+
+//     return totalBatteries;
+// }
+
+// console.log(getTotalAmountForBatteries(batteryBatches));
+
+let totalBatteries = batteryBatches.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue;
+});
+console.log(totalBatteries);
